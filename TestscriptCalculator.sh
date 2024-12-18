c6d5a16bbf925b6cde692d8cb8fd204b2a643e0d
kama <kama.djemel@gmail.com>
2024-12-18 16:24:13 +0100

    Utilisation du fichier Jenkinsfile TestscriptCalculator.sh v6

--- TestscriptCalculator.sh
+++ TestscriptCalculator.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "Debut du script"
 date
-echo "Creation du container claculator"
-docker run -d --name calculator --hostname calculator.gretadevops.com --net gretadevops.com -p 8080:8080 kbeber/calculator
+echo "Suppression du container calculator"
+docker rm -f calculator 2>/dev/null
+echo "Creation du container calculator"
+docker run -d --name calculator --hostname calculator.gretadevops.com --net gretadevops.com -p 8080:8080 kbeber/calculator  2>/dev/null
 sleep 10
-response=$(curl -s http://localhostcalculator.gretadevops.com:8080/sum?a=5\&b=6)
+response=$(curl -s http://calculator.gretadevops.com:8080/sum?a=5\&b=6)
 if  [ "$response" = "11" ] ; then
   	echo "ok"
  else 
